978f75015698f36ddf22ea16835c55b7b9de2648
Saunil Bajpai <saunilbajpai19@gmail.com>
2026-08-28 18:09:49 +0530

    feat: add SQL queries for Acacia species count and longest wheat genomic DNA sequence search

--- question2/queries.sql
+++ question2/queries.sql
@@ -4,3 +4,18 @@
 SELECT COUNT(DISTINCT species) AS acacia_species_count
 FROM taxonomy
 WHERE species LIKE 'Acacia%';
+
+
+-- Question 2B
+-- Find the wheat type associated with the longest genomic DNA sequence.
+
+SELECT
+    tx.species AS wheat_type,
+    rs.length AS dna_sequence_length
+FROM rfamseq AS rs
+JOIN taxonomy AS tx
+    ON tx.ncbi_id = rs.ncbi_id
+WHERE tx.species LIKE '%wheat%'
+  AND rs.mol_type = 'genomic DNA'
+ORDER BY rs.length DESC
+LIMIT 1;
