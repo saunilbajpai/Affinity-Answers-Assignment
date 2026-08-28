c4e6bd45d006309fdec7bbb035ae1e1c2dee74ea
Saunil Bajpai <saunilbajpai19@gmail.com>
2026-08-28 18:10:20 +0530

    feat: add SQL queries for taxonomy species counting, sequence analysis, and paginated family filtering

--- question2/queries.sql
+++ question2/queries.sql
@@ -19,3 +19,24 @@ WHERE tx.species LIKE '%wheat%'
   AND rs.mol_type = 'genomic DNA'
 ORDER BY rs.length DESC
 LIMIT 1;
+
+-- Question 2C
+-- Return page 9 (15 results per page) of families whose maximum
+-- associated genomic DNA sequence length exceeds 1,000,000.
+
+SELECT
+    f.rfam_acc AS family_accession,
+    f.rfam_id AS family_name,
+    MAX(rs.length) AS max_dna_sequence_length
+FROM family AS f
+JOIN full_region AS fr
+    ON fr.rfam_acc = f.rfam_acc
+JOIN rfamseq AS rs
+    ON rs.rfamseq_acc = fr.rfamseq_acc
+WHERE rs.mol_type = 'genomic DNA'
+GROUP BY
+    f.rfam_acc,
+    f.rfam_id
+HAVING MAX(rs.length) > 1000000
+ORDER BY max_dna_sequence_length DESC
+LIMIT 15 OFFSET 120;
